3c1fbcc46b2b6d7fd27cd2d45875e6225f6ffbc0
Eldhose Kurian <eldhosekurian9809@gmail.com>
2025-01-24 02:50:13 +0530

    serverUrlset

--- src/Pages/MenuItems/menuItems.jsx
+++ src/Pages/MenuItems/menuItems.jsx
@@ -1,24 +1,26 @@
-import "./menuItems.css";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import './menuItems.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+const serverUrl = process.env.REACT_APP_SERVER_URL;
 
 const MenuItems = () => {
   const [menu, setMenu] = useState([]);
 
   const fetchMenuLists = async () => {
-    const response = await axios.get(`http://localhost:7070/menu`);
+    // const response = await axios.get(`http://localhost:7070/menu`);
+    const response = await axios.get(`${serverUrl}/menu`);
     setMenu(response.data.menu);
   };
 
   useEffect(() => {
     fetchMenuLists();
   }, []);
-  console.log("mennnn", menu);
+  console.log('mennnn', menu);
 
   return (
     <div className="menuItems-main">
       <div className="menuContents">
-        {menu.map((item) => {
+        {menu.map(item => {
           return (
             <div className="rectangle" key={item._id}>
               <p>{item.menuName}</p>
