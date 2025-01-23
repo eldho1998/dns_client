fdc74c77339a3e44476b67160c4543580defb4ea
Eldhose Kurian <eldhosekurian9809@gmail.com>
2025-01-24 00:17:34 +0530

    fix

--- src/Pages/ItemList/itemList.jsx
+++ src/Pages/ItemList/itemList.jsx
@@ -1,6 +1,42 @@
-import "./itemList.css";
+import './itemList.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const ItemList = () => {
-  return <div className="itemList-main"></div>;
+  const [Items, setItems] = useState([]);
+
+  const fetchItemLists = async () => {
+    const response = await axios.get(`http://localhost:7070/item`);
+    setItems(response.data);
+  };
+
+  useEffect(() => {
+    fetchItemLists();
+  }, []);
+  console.log('mennnn', Items);
+
+  return (
+    <div className="itemList-main">
+      <div className="frames">
+        <div className="sideFrame"></div>
+        <div className="center-contents">
+          {Items.map(item => {
+            return (
+              <div key={item._id} className="lists">
+                <div className="ItemDescription">
+                  <h2>----- BRUNCH COCKTAILS -----</h2>
+                </div>
+                <div className="itemsLists">
+                  <h3>CINNAMON TOAST CRUNCH............$16</h3>
+                  <p>Descriptions</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="rightFrame"></div>
+      </div>
+    </div>
+  );
 };
 export default ItemList;
